e7c4083e3173443af34af1454ed650ab44385481
yasinxdxd <sesden19611@gmail.com>
2021-07-09 00:55:40 +0300

    dec_gfd_Clear function is created.

--- SRC/GFDDEC.C
+++ SRC/GFDDEC.C
@@ -17,8 +17,8 @@ int dec_gfdInit()
 void dec_gfdDestroy()
 {
     asm volatile(
-    "mov al, 0x03\n"
-    "int 0x21\n"
+    "mov ax, 0x03\n"	//80x16  text mode
+    "int 0x10\n"
     );
 
     //system("cls");
@@ -26,7 +26,7 @@ void dec_gfdDestroy()
 
 void dec_gfdDrawPixel(unsigned int x, unsigned int y)
 {
-    //if (x <= SCREEN_WIDTH && x >= 0 && y <= SCREEN_HEIGHT && y >= 0)
+    //if (x < 320 && x >= 0 && y < 200 && y >= 0)
     {
         asm volatile(
         "mov al, [0x3080]\n"
@@ -59,34 +59,44 @@ void dec_gfdSetPixelColor(unsigned char color)
 void dec_gfdDrawLine(float x0, float y0, float x1, float y1)
 {
     //DDA line algorithm
-    int delta_x = x1 - x0;
-    int delta_y = y1 - y0;
-    int steps = m_imax(m_iabs(delta_x), m_iabs(delta_y));  //we need to get max delta value to avoid gaps between pixels.
 
     float index_x, index_y;
-
-    float pixel_x = x0, pixel_y = y0;
     
+    x0 += 1.f;
+    y0 -= 1.f;
+    x1 += 1.f;
+    y1 -= 1.f;
+    
+    float pixel_x0 = SCREEN_WIDTH * x0 / 2.f;
+    float pixel_y0 = -SCREEN_HEIGHT * y0 / 2.f;
+    float pixel_x1 = SCREEN_WIDTH * x1 / 2.f;
+    float pixel_y1 = -SCREEN_HEIGHT * y1 / 2.f;
+
+    int delta_x = pixel_x1 - pixel_x0;
+    int delta_y = pixel_y1 - pixel_y0;
+    int steps = m_imax(m_iabs(delta_x), m_iabs(delta_y));  //we need to get max delta value to avoid gaps between pixels.
+
     index_y = delta_y / (float)steps;
     index_x = delta_x / (float)steps;
 
+
 #ifdef __dj_include_stdio_h_
-        printf("steps:%d\n", steps);
+        /*printf("steps:%d\n", steps);
         printf("ix:%f\n", index_x);
         printf("iy:%f\n", index_y);
         printf("fix:%d\n", (int)m_fround(index_x));
         printf("fiy:%d\n", (int)m_fround(index_y));
-		printf("7.9:%d\n", (int)m_fround(7.9));
+		printf("7.9:%d\n", (int)m_fround(7.9));*/
 #endif
 
         unsigned int i;
         for(i = 1; i <= steps; i++)
         {
 #ifndef __dj_include_stdio_h_            
-            dec_gfdDrawPixel(m_fround(pixel_x), m_fround(pixel_y));
+            dec_gfdDrawPixel(m_fround(pixel_x0), m_fround(pixel_y0));
 #endif
-            pixel_x += index_x;
-            pixel_y += index_y;
+            pixel_x0 += index_x;
+            pixel_y0 += index_y;
         }
 
 }
@@ -97,4 +107,24 @@ void dec_gfdDrawTriangle(float x0, float y0, float x1, float y1, float x2, float
     dec_gfdDrawLine(x0, y0, x1, y1);
     dec_gfdDrawLine(x1, y1, x2, y2);
     dec_gfdDrawLine(x2, y2, x0, y0);
+}
+
+void dec_gfdClear()
+{
+    /*
+    asm volatile(
+    "mov al, 0x13\n"       // 40x25 320x200 256 colors VGA 1
+    "mov ah, 0x0\n"
+    "int 0x10\n"           // apply configuration.
+    );
+    */
+    unsigned int i = 0;
+    for(i = 0; i < SCREEN_WIDTH; i++)
+    {
+        unsigned int j = 0;
+        for(j = 0; j < SCREEN_HEIGHT; j++)
+        {
+            dec_gfdDrawPixel(i, j);
+        }
+    }
 }
