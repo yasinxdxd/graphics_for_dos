6b6b64ad0a53175cb877a546fd1ffce0b5ab114d
Muhammed Yasinhan Yaşar <sesden19611@gmail.com>
2021-09-16 17:54:20 +0300

    Update GFDOS.C
    Added one more new function.

--- SRC/GFDOS.C
+++ SRC/GFDOS.C
@@ -10,6 +10,7 @@ GFD_CORE GFDDESTROYPROC graphics_for_dos_gfdDestroy = def_gfdDestroy;
 GFD_CORE GFDDRAWPIXELPROC graphics_for_dos_gfdDrawPixel = def_gfdDrawPixel;
 GFD_CORE GFDSETPIXELCOLORPROC graphics_for_dos_gfdSetPixelColor = def_gfdSetPixelColor;
 GFD_CORE GFDDRAWLINEPROC graphics_for_dos_gfdDrawLine = def_gfdDrawLine;
+GFD_CORE GFDDRAWLINEWITHOUTCOORDABSTRACTIONPROC graphics_for_dos_gfdDrawLineWithoutCoordAbstraction = def_gfdDrawLineWithoutCoordAbstraction;
 GFD_CORE GFDDRAWTRIANGLEPROC graphics_for_dos_gfdDrawTriangle = def_gfdDrawTriangle;
 GFD_CORE GFDDRAWTRIANGLEFILLEDPROC graphics_for_dos_gfdDrawTriangleFilled = def_gfdDrawTriangleFilled;
 GFD_CORE GFDCLEARPROC graphics_for_dos_gfdClear = def_gfdClear;
